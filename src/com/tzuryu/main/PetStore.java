80cd21cb7eeda562feff7d45ae43b0237c1d41cf
Marlon Monzon <martianworm17@gmail.com>
2019-04-20 14:47:32 +0800

    The Factory Pattern - I

---
+++ src/com/tzuryu/main/PetStore.java
@@ -0,0 +1,18 @@
+package com.tzuryu.main;
+
+public class PetStore {
+
+    //private Animal pet;
+
+    public static void main(String[] args) {
+        PetStore petStore = new PetStore();
+
+        //Animal pet = petStore.orderPet("Dollar", "Dog");
+        //pet.makeSound();
+        //pet.moveAround();
+    }
+
+//    public Animal orderPet(String petName, String petType) {
+//        return AnimalFactory.createAnimal(petName, petType);
+//    }
+}
